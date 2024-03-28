e8df90a8e33c12de312d7062903d3e1ab84c72f3
sonmac1203 <mactranthienson@gmail.com>
2024-03-28 09:11:40 -0700

    Add boilerplate page layout

---
+++ src/page-components/layouts/HomeLayout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+export const HomeLayout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>PickMeFood | Home</title>
+      </Head>
+      <main>
+        <div>{children}</div>
+      </main>
+    </>
+  );
+};
