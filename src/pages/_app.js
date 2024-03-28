6d08d6f742a8a4494e6f90dde8b0797e5900a308
sonmac1203 <mactranthienson@gmail.com>
2024-03-28 09:12:05 -0700

    Clean up _app.js and index.js (home)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
+import { HomeLayout } from "@/page-components/layouts";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }) => {
+  const PageLayout = Component.Layout || HomeLayout;
+
+  return (
+    <PageLayout {...pageProps}>
+      <Component {...pageProps} />
+    </PageLayout>
+  );
+};
+
+export default App;
