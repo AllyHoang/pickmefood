d350e9e0abfb66bb82b754d552a94e0c79485d98
Phan Anh <phananh12042003@gmail.com>
2024-04-27 13:22:11 -0400

    create landing page

--- src/page-components/layouts/HomeLayout.jsx
+++ src/page-components/layouts/HomeLayout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Head from "next/head";
+import GlobalNavbar from "../components/Navbar/GlobalNavbar";
 
 export const HomeLayout = ({ children }) => {
   return (
     <>
+      <GlobalNavbar></GlobalNavbar>
       <Head>
         <title>PickMeFood | Home</title>
       </Head>
