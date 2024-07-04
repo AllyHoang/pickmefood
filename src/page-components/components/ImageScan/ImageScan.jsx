a5410d23bb84235721876f7859b81835cf6d7a9d
sonmac1203 <mactranthienson@gmail.com>
2024-07-03 22:40:08 -0700

    updated styling of ImageScan

--- src/page-components/components/ImageScan/ImageScan.jsx
+++ src/page-components/components/ImageScan/ImageScan.jsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { CldUploadButton } from "next-cloudinary";
 import { useRouter } from "next/router";
-import styles from "./ImageScan.module.css";
 import { useImage } from "@/lib/ImageContext";
 import {
   Card,
@@ -10,9 +9,6 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { Car } from "lucide-react";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import ConfirmationPage from "../Confirmation/ConfirmationPage";
 import Image from "next/image";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import AddItem from "../addItemForm/addItemForm";
@@ -185,7 +181,7 @@ const ImageScan = ({ userId }) => {
       externalImageSource={uploadedImage}
     />
   ) : (
-    <div className="flex flex-col gap-6 overflow-auto h-full pb-16">
+    <div className="flex flex-col gap-6 overflow-auto h-full pb-16 max-h-screen">
       <div className="base-container">
         <Breadcrumbs
           crumbs={[
@@ -238,15 +234,15 @@ const ImageScan = ({ userId }) => {
           </CldUploadButton>
         )}
       </div>
-      <div className="grid grid-cols-2 base-container self-center gap-8 w-full flex-1 mt-2">
+      <div className="grid grid-cols-2 base-container self-center gap-8 w-full flex-1 mt-2 overflow-hidden">
         {uploadedImage && (
-          <div className="col-span-1">
+          <div className="col-span-1 p-1">
             <div className="w-full h-full relative">
               <Image
                 src={uploadedImage}
                 alt="Your uploaded photo"
                 fill
-                className="object-cover rounded object-top"
+                className="object-cover rounded-md object-top shadow-md"
               />
             </div>
           </div>
@@ -268,8 +264,8 @@ const ImageScan = ({ userId }) => {
             )}
           </div>
         ) : uploadedImage && detectedItems.length > 0 ? (
-          <div className="flex flex-col gap-8 h-full">
-            <Card className="flex flex-col max-h-72 overflow-hidden">
+          <div className="flex flex-col gap-8 h-full max-h-full overflow-hidden">
+            <Card className="flex flex-col max-h-72 overflow-hidden m-1">
               <CardHeader className="text-base-bold pb-2">
                 <CardTitle>Detected items</CardTitle>
                 <CardDescription
@@ -314,11 +310,26 @@ const ImageScan = ({ userId }) => {
                 </ul>
               </div>
             </Card>
-            <Card className="flex flex-col flex-1 overflow-hidden">
-              <CardHeader className="text-base-bold">
-                <CardTitle>Confirmed items</CardTitle>
+            <Card className="flex flex-col flex-1 overflow-hidden m-1 gap-4">
+              <CardHeader className="text-base-bold pb-0">
+                <CardTitle>
+                  Confirmed items{" "}
+                  {[...confirmedItems].length > 0 && (
+                    <span className="text-muted-foreground text-small-medium">
+                      ({[...confirmedItems].length})
+                    </span>
+                  )}
+                </CardTitle>
+                <CardDescription
+                  className="text-small-medium"
+                  style={{ fontWeight: "400" }}
+                >
+                  <p className="text-muted-foreground">
+                    Scroll to see more items
+                  </p>
+                </CardDescription>
               </CardHeader>
-              <div className="flex-1 overflow-y-scroll px-6 max-h-64">
+              <div className="flex-1 overflow-y-scroll px-6">
                 {[...confirmedItems].length === 0 ? (
                   <div className="h-full flex flex-col justify-center">
                     <p className="text-center text-muted-foreground">
@@ -334,7 +345,7 @@ const ImageScan = ({ userId }) => {
                           index !== [...confirmedItems].length - 1
                             ? "border-b border-gray-300 py-2"
                             : "pt-2 pb-4"
-                        }`}
+                        } ${index === 0 && "pt-0"}`}
                       >
                         {item}
                       </li>
@@ -342,26 +353,12 @@ const ImageScan = ({ userId }) => {
                   </ul>
                 )}
               </div>
-              {/* <Dialog>
-                    <DialogTrigger>
-                      <Button
-                        onClick={redirectToConfirmationPage}
-                        className=" bg-sky-400 mb-4 w-[93%]"
-                        disabled={[...confirmedItems].length === 0}
-                      >
-                        Go to confirmation page
-                      </Button>
-                    </DialogTrigger>
-                    <DialogContent className="min-w-[80%] min-h-fit overflow-auto">
-                      <ConfirmationPage userId={userId}></ConfirmationPage>
-                    </DialogContent>
-                  </Dialog> */}
               <Button
                 onClick={redirectToConfirmationPage}
                 className="bg-sky-400 mx-6 mt-auto mb-4"
                 disabled={[...confirmedItems].length === 0}
               >
-                Go to confirmation page
+                Confirm your items
               </Button>
             </Card>
           </div>
